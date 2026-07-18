1def7533560c1f211b6addb59997a548620954c3
Thar Htet Aung <tharhtetaung724@gmail.com>
2026-07-18 12:12:16 +0700

    code change for new file

--- arithmetic_operate.py
+++ arithmetic_operate.py
@@ -3,16 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    img = cv.imread('images/lena_color_512.tif', 1)
+    img = cv.imread('images/lena_color_512.tif')
+    img2 = cv.imread('images/lake.tif')
 
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB) #type: ignore
+    img = cv.cvtColor(img,cv.COLOR_BGR2RGB) #type:ignore
+    img2 = cv.cvtColor(img2,cv.COLOR_RGB2BGR) #type:ignore
 
+    plt.subplot(1,2,1)
     plt.imshow(img)
+    plt.title('Lena')
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.subplot(1,2,2)
+    plt.imshow(img2)
+    plt.title('Lake')
+    plt.xticks([])
+    plt.yticks([])
+
     plt.show()
-
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main()
